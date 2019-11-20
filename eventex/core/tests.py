09850eec01cc1ff115586f5fcf1804937eb7456a
Rafael Nunes <rafaelscnunes@gmail.com>
2019-11-20 03:57:40 -0300

    Habilita inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -7,10 +7,11 @@ class HomeTest(TestCase):
 
     def test_get(self):
         """GET / must return status code 200"""
-
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
         """Must use index.html"""
-
         self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
