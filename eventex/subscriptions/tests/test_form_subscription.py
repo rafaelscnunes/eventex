a234eeda39908b7d9571790630fbd9e8e6ae6813
Rafael Nunes <rafaelscnunes@gmail.com>
2019-12-10 00:26:34 -0300

    e-mail ou telefone opcionais

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -43,3 +43,18 @@ class SubscriptionFormTest(TestCase):
         # JOSÉ das Couves -> José Das Couves
         form = self.make_validated_form(name='JOSÉ das Couves')
         self.assertEqual('José Das Couves', form.cleaned_data['name'])
+
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
